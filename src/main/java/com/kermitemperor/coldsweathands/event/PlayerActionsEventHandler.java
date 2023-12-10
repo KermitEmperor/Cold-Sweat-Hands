d62d4a952b2b1a1049988c7014dc611926f65d67
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-12-10 20:34:38 +0100

    Functionality implemented on RightClickEvent

--- src/main/java/com/kermitemperor/coldsweathands/event/PlayerActionsEventHandler.java
+++ src/main/java/com/kermitemperor/coldsweathands/event/PlayerActionsEventHandler.java
@@ -6,12 +6,12 @@ import com.momosoftworks.coldsweat.api.util.Temperature;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import static com.kermitemperor.coldsweathands.ColdSweatHands.LOGGER;
 
 
 @Mod.EventBusSubscriber(modid = ColdSweatHands.MOD_ID)
@@ -23,17 +23,33 @@ public class PlayerActionsEventHandler {
 
         if (entity != null && entity.getType().equals(EntityType.PLAYER)) {
             Player player = (Player) entity;
-            ItemInfo HandItemInfo = new ItemInfo(player.getMainHandItem().getItem());
-            //ItemInfo BlockItemInfo = new ItemInfo(entity.block);
             double tempAroundPlayer = Temperature.get(player, Temperature.Type.WORLD);
-            double convertedTempAroundPlayer = Temperature.convertUnits(tempAroundPlayer, Temperature.Units.MC, HandItemInfo.getMeasure(), true);
 
-            //LOGGER.info(BlockItemInfo.getRegistryName().toString());
-            LOGGER.info(String.valueOf(convertedTempAroundPlayer));
-            LOGGER.info(String.valueOf(HandItemInfo.getMin()));
+            ItemInfo handItemInfo = new ItemInfo(player.getMainHandItem().getItem());
+            Block targetBlock = player.getLevel().getBlockState(event.getHitVec().getBlockPos()).getBlock();
+            ItemInfo targetBlockInfo = new ItemInfo(targetBlock.asItem());
 
-            if ((null != HandItemInfo.getMin()) && HandItemInfo.getMin() > convertedTempAroundPlayer) {
-                event.setUseItem(Event.Result.DENY);
+
+
+            if (handItemInfo.getInfo() != null) {
+                double convertedTempAroundPlayer = Temperature.convertUnits(tempAroundPlayer, Temperature.Units.MC, handItemInfo.getMeasure(), true);
+                if ((null != handItemInfo.getMin()) && handItemInfo.getMin() > convertedTempAroundPlayer) {
+                    event.setUseItem(Event.Result.DENY);
+                }
+                if ((null != handItemInfo.getMax()) && handItemInfo.getMax() < convertedTempAroundPlayer) {
+                    event.setUseItem(Event.Result.DENY);
+                }
+            }
+            if (targetBlockInfo.getInfo() != null) {
+                double convertedTempAroundPlayer = Temperature.convertUnits(tempAroundPlayer, Temperature.Units.MC, targetBlockInfo.getMeasure(), true);
+                if ((targetBlockInfo.getClickable() != null) && targetBlockInfo.getClickable()) {
+                    if ((null != targetBlockInfo.getMin()) && targetBlockInfo.getMin() > convertedTempAroundPlayer) {
+                        event.setUseBlock(Event.Result.DENY);
+                    }
+                    if ((null != targetBlockInfo.getMax()) && targetBlockInfo.getMax() < convertedTempAroundPlayer) {
+                        event.setUseBlock(Event.Result.DENY);
+                    }
+                }
             }
 
 
